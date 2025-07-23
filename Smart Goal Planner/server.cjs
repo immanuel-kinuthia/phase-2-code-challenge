fb8d5d8e6c3c1efbb1de2d69a8b844870a5ccf62
Immanuel Kinuthia <immanuel.kinuthia@student.moringaschool.com>
2025-07-23 14:14:11 +0300

    Render Edit

--- Smart Goal Planner/server.cjs
+++ Smart Goal Planner/server.cjs
@@ -4,16 +4,17 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
 server.use(middlewares);
-server.use(router);
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-  console.log(`JSON Server is running on port ${port}`);
-});
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
+});
+
+server.use(router);
+
+const port = process.env.PORT || 3000;
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
 });
