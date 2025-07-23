6c7a741f1f478f155f51d41e93aadfbe07f3ad82
Immanuel Kinuthia <immanuel.kinuthia@student.moringaschool.com>
2025-07-23 13:52:28 +0300

    Render Edit

--- Smart Goal Planner/server.cjs
+++ Smart Goal Planner/server.cjs
@@ -10,3 +10,10 @@ const port = process.env.PORT || 3000;
 server.listen(port, () => {
   console.log(`JSON Server is running on port ${port}`);
 });
+
+server.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
